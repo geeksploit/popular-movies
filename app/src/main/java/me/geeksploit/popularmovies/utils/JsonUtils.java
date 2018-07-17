a6b732115bfe4c4f70aa6e3ce34cca5cabb19e34
Max Rem <max@geeksploit.me>
2018-07-17 17:28:03 +0300

    feat: Add a JSON contract according to TMDb API

--- app/src/main/java/me/geeksploit/popularmovies/utils/JsonUtils.java
+++ app/src/main/java/me/geeksploit/popularmovies/utils/JsonUtils.java
@@ -2,3 +2,13 @@ package me.geeksploit.popularmovies.utils;
 
 public final class JsonUtils {
 }
+
+final class JsonContractTheMovieDb {
+    final static String RESULTS = "results";
+
+    final static String VOTE_AVERAGE = "vote_average";
+    final static String POSTER_PATH = "poster_path";
+    final static String TITLE = "title";
+    final static String OVERVIEW = "overview";
+    final static String RELEASE_DATE = "release_date";
+}
