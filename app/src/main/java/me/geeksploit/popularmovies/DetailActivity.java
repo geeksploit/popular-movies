7060541edc517743c5399131665725388f0476ae
Max Rem <max@geeksploit.me>
2018-07-17 19:14:13 +0300

    feat: Add a helper method that populates TextView

--- app/src/main/java/me/geeksploit/popularmovies/DetailActivity.java
+++ app/src/main/java/me/geeksploit/popularmovies/DetailActivity.java
@@ -2,6 +2,7 @@ package me.geeksploit.popularmovies;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+    }
+
+    private void setText(int id, String text) {
+        TextView textView = findViewById(id);
+        textView.setText(text);
     }
 }
