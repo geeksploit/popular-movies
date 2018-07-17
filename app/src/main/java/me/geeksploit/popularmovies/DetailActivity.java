370aa9dba3b64dad9271cd3292701dd4f7e01c98
Max Rem <max@geeksploit.me>
2018-07-17 19:19:49 +0300

    feat: Obtain movie data from a calling activity

--- app/src/main/java/me/geeksploit/popularmovies/DetailActivity.java
+++ app/src/main/java/me/geeksploit/popularmovies/DetailActivity.java
@@ -1,15 +1,26 @@
 package me.geeksploit.popularmovies;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import me.geeksploit.popularmovies.model.MovieModel;
+
 public class DetailActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MOVIE = "MOVIE_DATA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        if (intent.getExtras() == null) return;
+
+        final MovieModel movie = (MovieModel) intent.getExtras().get(EXTRA_MOVIE);
+        if (movie == null) return;
     }
 
     private void setText(int id, String text) {
