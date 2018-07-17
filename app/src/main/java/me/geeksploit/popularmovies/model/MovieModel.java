1fe01aa8ff75c0e951b274eca9bbf569615798f8
Max Rem <max@geeksploit.me>
2018-07-17 17:18:28 +0300

    feat: Add a movie model class stub

---
+++ app/src/main/java/me/geeksploit/popularmovies/model/MovieModel.java
@@ -0,0 +1,16 @@
+package me.geeksploit.popularmovies.model;
+
+/**
+ * Holds detailed movie information.
+ * <p>
+ * Stage 1 requirements:
+ * <ul>
+ * <li>title
+ * <li>movie poster image thumbnail
+ * <li>a plot synopsis (called overview in the api)
+ * <li>user rating (called vote_average in the api)
+ * <li>release date
+ * </ul>
+ */
+public class MovieModel {
+}
