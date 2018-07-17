b15be47ba3a665e0a49cc21f1bf08e82475d7083
geeksploit <geeksploit@local>
2018-07-17 19:44:40 +0300

    feat: Initialize movie grid
    Resolves: #1

--- app/src/main/java/me/geeksploit/popularmovies/MainActivity.java
+++ app/src/main/java/me/geeksploit/popularmovies/MainActivity.java
@@ -1,15 +1,25 @@
 package me.geeksploit.popularmovies;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import me.geeksploit.popularmovies.model.MovieModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GridView moviesGrid;
+
+    private MovieModel[] mMovies = new MovieModel[]{};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,24 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        initializeViews();
+    }
+
+    private void initializeViews() {
+        moviesGrid = findViewById(R.id.movies_grid);
+        moviesGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Intent movieDetail = new Intent(MainActivity.this, DetailActivity.class);
+                movieDetail.putExtra(DetailActivity.EXTRA_MOVIE, mMovies[position]);
+                startActivity(movieDetail);
+            }
+        });
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
+            moviesGrid.setNumColumns(5);
+        else
+            moviesGrid.setNumColumns(3);
     }
 
     @Override
