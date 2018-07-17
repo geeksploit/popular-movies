6c0b40be0aaa17ebea525c7de5178fd3645fd0ca
Max Rem <max@geeksploit.me>
2018-07-17 15:56:40 +0300

    feat: Add a method that checks if network is connected

--- app/src/main/java/me/geeksploit/popularmovies/utils/NetworkUtils.java
+++ app/src/main/java/me/geeksploit/popularmovies/utils/NetworkUtils.java
@@ -1,4 +1,29 @@
 package me.geeksploit.popularmovies.utils;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 public final class NetworkUtils {
+
+    public static boolean haveNetworkConnection(Context context) {
+        // TODO: is there a better way to check if the Internet connection is up and running?
+
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm == null) return false;
+
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getType() == ConnectivityManager.TYPE_WIFI)
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getType() == ConnectivityManager.TYPE_MOBILE)
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return haveConnectedWifi || haveConnectedMobile;
+    }
 }
