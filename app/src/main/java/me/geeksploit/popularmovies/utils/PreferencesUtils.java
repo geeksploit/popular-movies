042bf85b9b5bd52e90e23675fc7c9091a968bef9
geeksploit <geeksploit@local>
2018-07-17 18:04:21 +0300

    feat: Add default preferences getter

--- app/src/main/java/me/geeksploit/popularmovies/utils/PreferencesUtils.java
+++ app/src/main/java/me/geeksploit/popularmovies/utils/PreferencesUtils.java
@@ -1,5 +1,9 @@
 package me.geeksploit.popularmovies.utils;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 public class PreferencesUtils {
 
     private static final String PREF_KEY_API = "api_key";
@@ -9,5 +13,9 @@ public class PreferencesUtils {
     private static final String PREF_VALUE_SORT_POPULAR = "popular";
     private static final String PREF_VALUE_SORT_TOP_RATED = "top_rated";
     private static final String PREF_VALUE_SORT_DEFAULT = PREF_VALUE_SORT_POPULAR;
+
+    private static SharedPreferences getPreferences(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context);
+    }
 
 }
