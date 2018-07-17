0eb17c33989b28d514306af6689bb3adabc2ae8b
geeksploit <geeksploit@local>
2018-07-17 18:02:43 +0300

    feat: Define preference related constants

--- app/src/main/java/me/geeksploit/popularmovies/utils/PreferencesUtils.java
+++ app/src/main/java/me/geeksploit/popularmovies/utils/PreferencesUtils.java
@@ -1,4 +1,13 @@
 package me.geeksploit.popularmovies.utils;
 
 public class PreferencesUtils {
+
+    private static final String PREF_KEY_API = "api_key";
+    private static final String PREF_VALUE_API_DEFAULT = "";
+
+    private static final String PREF_KEY_SORT_MODE = "sort_mode";
+    private static final String PREF_VALUE_SORT_POPULAR = "popular";
+    private static final String PREF_VALUE_SORT_TOP_RATED = "top_rated";
+    private static final String PREF_VALUE_SORT_DEFAULT = PREF_VALUE_SORT_POPULAR;
+
 }
