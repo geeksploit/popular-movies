d537a5db2f42be0030fbb3cbb85fae130c893449
geeksploit <geeksploit@local>
2018-07-22 18:29:34 +0300

    feat: Extend RecyclerView.Adapter from movie gallery

--- app/src/main/java/me/geeksploit/popularmovies/adapter/MovieGalleryRecyclerAdapter.java
+++ app/src/main/java/me/geeksploit/popularmovies/adapter/MovieGalleryRecyclerAdapter.java
@@ -1,12 +1,30 @@
 package me.geeksploit.popularmovies.adapter;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import me.geeksploit.popularmovies.R;
 
-public class MovieGalleryRecyclerAdapter {
+public class MovieGalleryRecyclerAdapter extends RecyclerView.Adapter<MovieGalleryRecyclerAdapter.MovieGalleryViewHolder> {
+
+    @NonNull
+    @Override
+    public MovieGalleryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieGalleryViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     class MovieGalleryViewHolder extends RecyclerView.ViewHolder {
 
