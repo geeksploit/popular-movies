a13e9af688e71c2db88209a06a62ad30646631ba
Max Rem <max@geeksploit.me>
2018-07-22 22:04:27 +0300

    feat: Create review model constructor

--- app/src/main/java/me/geeksploit/popularmovies/model/ReviewModel.java
+++ app/src/main/java/me/geeksploit/popularmovies/model/ReviewModel.java
@@ -1,4 +1,16 @@
 package me.geeksploit.popularmovies.model;
 
 public final class ReviewModel {
+
+    private final String author;
+    private final String content;
+    private final String url;
+
+    public ReviewModel(String author,
+                       String content,
+                       String url) {
+        this.author = author;
+        this.content = content;
+        this.url = url;
+    }
 }
