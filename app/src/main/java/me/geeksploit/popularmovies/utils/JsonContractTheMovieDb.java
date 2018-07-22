8770ede92b4f7fad07582e4fea10472987d48054
geeksploit <geeksploit@local>
2018-07-22 12:59:57 +0300

    style: Extract a contract class into a separate file

---
+++ app/src/main/java/me/geeksploit/popularmovies/utils/JsonContractTheMovieDb.java
@@ -0,0 +1,12 @@
+package me.geeksploit.popularmovies.utils;
+
+final class JsonContractTheMovieDb {
+    final static String RESULTS = "results";
+
+    final static String VOTE_AVERAGE = "vote_average";
+    final static String POSTER_PATH = "poster_path";
+    final static String TITLE = "title";
+    final static String OVERVIEW = "overview";
+    final static String RELEASE_DATE = "release_date";
+}
+
