b608c19153803a1cc84e9d8d9d1b57aa91c401c1
Max Rem <max@geeksploit.me>
2018-07-22 18:27:15 +0300

    feat: Implement movie gallery item ViewHolder

--- app/src/main/java/me/geeksploit/popularmovies/adapter/MovieGalleryRecyclerAdapter.java
+++ app/src/main/java/me/geeksploit/popularmovies/adapter/MovieGalleryRecyclerAdapter.java
@@ -1,4 +1,20 @@
 package me.geeksploit.popularmovies.adapter;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+
+import me.geeksploit.popularmovies.R;
+
 public class MovieGalleryRecyclerAdapter {
+
+    class MovieGalleryViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView moviePoster;
+
+        MovieGalleryViewHolder(View itemView) {
+            super(itemView);
+            moviePoster = itemView.findViewById(R.id.movieThumbnail);
+        }
+    }
 }
