85c627060a886b297673f4823be14bf24adc5e58
geeksploit <geeksploit@local>
2018-07-22 20:47:10 +0300

    feat: Implement movie gallery item click listener

--- app/src/main/java/me/geeksploit/popularmovies/adapter/MovieGalleryAdapter.java
+++ app/src/main/java/me/geeksploit/popularmovies/adapter/MovieGalleryAdapter.java
@@ -8,20 +8,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import me.geeksploit.popularmovies.R;
 import me.geeksploit.popularmovies.model.MovieModel;
 import me.geeksploit.popularmovies.utils.NetworkUtils;
 
-public class MovieGalleryRecyclerAdapter extends RecyclerView.Adapter<MovieGalleryRecyclerAdapter.MovieGalleryViewHolder> {
+public class MovieGalleryAdapter extends RecyclerView.Adapter<MovieGalleryAdapter.MovieGalleryViewHolder> {
 
     private Context context;
     private List<MovieModel> movieList;
+    private OnClickListener onClickListener;
 
-    public MovieGalleryRecyclerAdapter(Context context, List<MovieModel> movieList) {
+    public MovieGalleryAdapter(Context context, OnClickListener onClickListener) {
         this.context = context.getApplicationContext();
-        this.movieList = movieList;
+        this.movieList = new ArrayList<>();
+        this.onClickListener = onClickListener;
     }
 
     @NonNull
@@ -29,7 +32,7 @@ public class MovieGalleryRecyclerAdapter extends RecyclerView.Adapter<MovieGalle
     public MovieGalleryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.movie_grid_item, parent, false);
-        return new MovieGalleryViewHolder(view);
+        return new MovieGalleryViewHolder(view, onClickListener);
     }
 
     @Override
@@ -45,13 +48,26 @@ public class MovieGalleryRecyclerAdapter extends RecyclerView.Adapter<MovieGalle
         return movieList.size();
     }
 
-    class MovieGalleryViewHolder extends RecyclerView.ViewHolder {
+    public interface OnClickListener {
+        void onClick(MovieModel movie);
+    }
+
+    class MovieGalleryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         ImageView moviePoster;
+        private OnClickListener onClickListener;
 
-        MovieGalleryViewHolder(View itemView) {
+        MovieGalleryViewHolder(View itemView, MovieGalleryAdapter.OnClickListener onClickListener) {
             super(itemView);
             moviePoster = itemView.findViewById(R.id.movieThumbnail);
+
+            this.onClickListener = onClickListener;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            onClickListener.onClick(movieList.get(getAdapterPosition()));
         }
     }
 }
