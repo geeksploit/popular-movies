a228403fbc55b7c9dd77b3cde4d491a7a90facca
Max Rem <max@geeksploit.me>
2018-07-23 10:43:51 +0300

    feat: Add a method to parse a review from JSON

--- app/src/main/java/me/geeksploit/popularmovies/utils/JsonUtils.java
+++ app/src/main/java/me/geeksploit/popularmovies/utils/JsonUtils.java
@@ -5,10 +5,31 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import me.geeksploit.popularmovies.model.MovieModel;
+import me.geeksploit.popularmovies.model.ReviewModel;
 
-import static me.geeksploit.popularmovies.utils.JsonContractTheMovieDb.*;
+import static me.geeksploit.popularmovies.utils.JsonContractTheMovieDb.OVERVIEW;
+import static me.geeksploit.popularmovies.utils.JsonContractTheMovieDb.POSTER_PATH;
+import static me.geeksploit.popularmovies.utils.JsonContractTheMovieDb.RELEASE_DATE;
+import static me.geeksploit.popularmovies.utils.JsonContractTheMovieDb.RESULTS;
+import static me.geeksploit.popularmovies.utils.JsonContractTheMovieDb.REVIEW_AUTHOR;
+import static me.geeksploit.popularmovies.utils.JsonContractTheMovieDb.REVIEW_CONTENT;
+import static me.geeksploit.popularmovies.utils.JsonContractTheMovieDb.REVIEW_URL;
+import static me.geeksploit.popularmovies.utils.JsonContractTheMovieDb.TITLE;
+import static me.geeksploit.popularmovies.utils.JsonContractTheMovieDb.VOTE_AVERAGE;
 
 public final class JsonUtils {
+
+    public static JSONArray getResults(String json) throws JSONException {
+        return new JSONObject(json).getJSONArray(RESULTS);
+    }
+
+    public static ReviewModel parseReview(JSONObject jsonObject) throws JSONException {
+        return new ReviewModel(
+                jsonObject.getString(REVIEW_AUTHOR),
+                jsonObject.getString(REVIEW_CONTENT),
+                jsonObject.getString(REVIEW_URL)
+        );
+    }
 
     public static MovieModel[] parseTheMovieDb(String json) {
         if (json == null) return null;
