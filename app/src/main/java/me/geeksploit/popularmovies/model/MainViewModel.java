7409c4a066c4cfa9648716c2ed7e2ce1010c9f5e
Max Rem <max@geeksploit.me>
2018-07-27 22:09:04 +0300

    feat: Initialize main data sources

--- app/src/main/java/me/geeksploit/popularmovies/model/MainViewModel.java
+++ app/src/main/java/me/geeksploit/popularmovies/model/MainViewModel.java
@@ -2,11 +2,26 @@ package me.geeksploit.popularmovies.model;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.support.annotation.NonNull;
+
+import java.util.List;
+
+import me.geeksploit.popularmovies.database.AppDatabase;
 
 public final class MainViewModel extends AndroidViewModel {
 
+    private final LiveData<List<MovieModel>> mMoviesFromLocalFavorites;
+    private final MutableLiveData<List<MovieModel>> mMoviesFromNetworkApi;
+    private final MutableLiveData<LiveData<List<MovieModel>>> mMovieSource;
+
     public MainViewModel(@NonNull Application application) {
         super(application);
+        AppDatabase db = AppDatabase.getInstance(getApplication());
+        mMoviesFromLocalFavorites = db.movieDao().loadAllMovies();
+        mMoviesFromNetworkApi = new MutableLiveData<>();
+        mMovieSource = new MutableLiveData<>();
     }
+
 }
