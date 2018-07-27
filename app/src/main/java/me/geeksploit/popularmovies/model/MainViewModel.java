96a5c51b2b8780597f3c163427abb98466ce2ba8
Max Rem <max@geeksploit.me>
2018-07-27 22:07:24 +0300

    feat: Add main gallery ViewModel stub

---
+++ app/src/main/java/me/geeksploit/popularmovies/model/MainViewModel.java
@@ -0,0 +1,12 @@
+package me.geeksploit.popularmovies.model;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+public final class MainViewModel extends AndroidViewModel {
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
